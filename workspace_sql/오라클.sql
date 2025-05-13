74e724a3376101156e224c9bd4f404e165ed72f5
조예주 <dPwn123488@gmail.com>
2025-05-13 16:56:51 +0900

    연산자 추가

--- workspace_sql/오라클.sql
+++ workspace_sql/오라클.sql
@@ -67,11 +67,87 @@ select *
     from emp
   order by deptno asc, sal desc, empno desc;
 
+-- 부서 번호가 30인 데이터만 출력
+select *
+        from emp
+    where deptno = 30;
 
+select *
+        from emp
+    where deptno = 30
+  order by sal;
 
+select *
+    from emp
+  where empno = 7782;
 
+-- sal이 1600인 사람만 출력
+select *
+    from emp
+  where sal = 1600;
 
+-- 1600 초과
+select *
+    from emp
+  where sal > 1600;
 
+-- 1600 이상
+select *
+    from emp
+  where sal >= 1600; -- =>는 없음, = 이 오른쪽
+
+-- 1600 이하
+select *
+    from emp
+  where sal <= 1600;
+
+-- 부정(!,C언어 등)
+select *
+    from emp
+  where deptno != 30;
+-- 부정(오라클)  
+select *
+    from emp
+  where deptno <> 30;
+
+select * from emp
+where deptno = 30
+and job = 'SALESMAN';
+
+-- OR
+/*
+    emp 테이블에서 deptno가 30 이거나(또는) 
+    job이 CLERK인 사원의 모든 정보를 출력 
+*/
+select *
+from emp
+where
+    deptno = 30 or job = 'CLERK';
+
+/*
+    job이 CLERK이거나 sal이 1000 초과이면서 deptno가 10인 사원 조회
+*/
+select * from emp
+where (job = 'CLERK' or sal > 1000) and deptno = 10;
+
+select * from emp
+where not (job = 'CLERK' or ( sal > 1000 and deptno = 10 ));
+
+/*
+    deptno != 30, deptno <> 30, deptno ^= 30, not deptno =30
+*/
+-- 월급 2000이상 4000미만인 사원 출력
+select * from emp
+where sal >= 2000 and sal < 4000;
+-- select * from emp where 2000 <= sal and sal < 4000;
+ 
+-- 부서번호 10 또는 20인 사원만 출력
+select * from emp
+    where deptno = 10 or deptno = 20;
+select * from emp
+    where deptno in ( 10, 20 );
+select * from emp
+    where deptno not in ( 10, 20 );
 
 
 
